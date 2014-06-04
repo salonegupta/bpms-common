066108c9d093e919586cda0996895e73746e5a1a
Jayant Vaish <vaish@intalio.com>
2014-06-04 16:48:48 +0530

    Implementing BPMS-1161

--- src/com/intalio/bpms/common/AxisUtil.java
+++ src/com/intalio/bpms/common/AxisUtil.java
@@ -1,25 +1,31 @@
 package com.intalio.bpms.common;
 
 import org.apache.axis2.AxisFault;
+import org.apache.axis2.Constants;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.description.TransportOutDescription;
 import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.commons.httpclient.HttpClient;
 
 public class AxisUtil {
 
-	public Options getDefaultOptions(){
-		Options options = new Options();
-		options.setProperty(HTTPConstants.REUSE_HTTP_CLIENT, org.apache.axis2.Constants.VALUE_TRUE);
-		return options ;
-	}
-	
-	
-	public ServiceClient getServiceClient() throws AxisFault {
-		HttpClient httpClient = new HttpClient(MultiThreadedHttpConnectionManagerFactory.getInstance());
-		ServiceClient sc = new ServiceClient(null, null);
-		sc.setOptions(getDefaultOptions());
+    public ServiceClient getServiceClient() throws AxisFault {
+        return getServiceClient(new Options());
+    }
 
+    public ServiceClient getServiceClient(Options options) throws AxisFault {
+        return getServiceClient(options, true);
+    }
+
+    public ServiceClient getServiceClient(Options options, boolean isLocalCall) throws AxisFault {
+        HttpClient httpClient = new HttpClient(
+                MultiThreadedHttpConnectionManagerFactory.getInstance());
+        ServiceClient sc = new ServiceClient(null, null);
+        sc.setOptions(options);
+
+        sc.getOptions().setProperty(HTTPConstants.REUSE_HTTP_CLIENT,
+                org.apache.axis2.Constants.VALUE_TRUE);
         /*
          * Fix for PXEI-917: Earlier we were setting httpclient in axis2's
          * configContext due to issue of cleanup of idle http thread. Now as
@@ -30,8 +36,13 @@ public class AxisUtil {
          */
         sc.getOptions().setProperty(HTTPConstants.CACHED_HTTP_CLIENT,
                 httpClient);
-		return sc;
-	}
+        if (isLocalCall) {
+            TransportOutDescription tOut = sc.getAxisConfiguration()
+                    .getTransportOut(Constants.TRANSPORT_LOCAL);
+            sc.getOptions().setTransportOut(tOut);
+        }
+        return sc;
+    }
 
 	public void closeClient(ServiceClient sc) throws AxisFault {
 		if (sc != null) {
